89012896dca6634397428a737b558f139ed7efea
Piccoli <marco.piccoli@hpc-head-n1.unitn.it>
2025-10-07 10:25:43 +0200

    First commit for configuration vscode remote

---
+++ src/prova.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+int main(){
+    int i;
+
+    for(i = 0; i < 10; i++)
+    {
+        cout << "This is just an output of " + i;
+    }
+    return 0;
+}
