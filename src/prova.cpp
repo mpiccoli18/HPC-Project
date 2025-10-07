3fc0ee9664822920f03c16e37473a1ddd6c6b11b
Piccoli <marco.piccoli@hpc-head-n1.unitn.it>
2025-10-07 10:32:33 +0200

    Another comm

--- src/prova.cpp
+++ src/prova.cpp
@@ -3,10 +3,11 @@
 using namespace std;
 int main(){
     int i;
-
+    int j = 100;
     for(i = 0; i < 10; i++)
     {
         cout << "This is just an output of " + i;
+        cout << endl << "Also, there is " + j << "endl";
     }
     return 0;
 }
