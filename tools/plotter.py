a2aa75e61e396a77108ba00a1b0c240daf532d58
mpiccoli18 <mpiccoli18@gmail.com>
2025-12-19 16:52:17 +0100

    Added the feature of plotting in 2d, and also the possibility to save the resulting plot (both in 2d and 3d) as a .png transparent image file

--- tools/plotter.py
+++ tools/plotter.py
@@ -1,6 +1,30 @@
 import numpy as np
 import pyvista as pv
+import matplotlib.pyplot as plt
 
+def plot_points2d(points: np.ndarray):
+    # Take only X and Y (columns 0 and 1)
+    x = points[:, 0]
+    y = points[:, 1]
+    labels = points[:, 3]
+
+    plt.figure(figsize=(10, 8))
+
+    # Create scatter plot
+    scatter = plt.scatter(x, y, c=labels, cmap='jet', s=7.5, edgecolors='none')
+
+    # Add a colorbar
+    plt.colorbar(scatter, label='Values')
+
+    plt.xlabel('X Axis')
+    plt.ylabel('Y Axis')
+    plt.title('Spectral clustering with 500 data points')
+
+
+    # Save with transparency
+    #plt.savefig('plot1.png', transparent=True)
+    plt.show()
+    plt.close()
 
 def parse_points(file) -> np.ndarray:
     lines = file.read().strip().splitlines()
@@ -14,7 +38,7 @@ def parse_points(file) -> np.ndarray:
     return np.array(data)
 
 
-def plot_points(points: np.ndarray):
+def plot_points3d(points: np.ndarray):
     coords = points[:, :3].astype(float)
     labels = points[:, 3].astype(float)
 
@@ -22,10 +46,10 @@ def plot_points(points: np.ndarray):
     cloud["labels"] = labels
     print(f"Coords shape: {coords.shape}") # Should be (Number, 3)
 
-    sphere_geom = pv.Sphere(radius=0.15) # Adjust radius based on your data scale
+    sphere_geom = pv.Sphere(radius=0.125) # Adjust radius based on your data scale
     geom_points = cloud.glyph(geom=sphere_geom, scale=False)
 
-    plotter = pv.Plotter(off_screen=True)
+    plotter = pv.Plotter()
     plotter.set_background("white")
     plotter.add_mesh(
         geom_points,
@@ -34,14 +58,16 @@ def plot_points(points: np.ndarray):
         smooth_shading=True
     )
 
+    plotter.view_xy()
     plotter.add_axes()
     plotter.camera_set = True
     plotter.reset_camera()
+    plotter.show()
 
     print(f"Data range: {coords.min(axis=0)} to {coords.max(axis=0)}")
     print(f"Plotter bounds: {plotter.bounds}")
 
-    plotter.screenshot("plot2.png", transparent_background=True)
+    #plotter.screenshot("plot4.png", transparent_background=True)
     plotter.close()
 
 
@@ -61,7 +87,8 @@ def main():
     with open(pargs.input_file, 'r') as file:
         points = parse_points(file)
         print("Opened file: %s", file)
-        plot_points(points)
+        plot_points3d(points)
+        plot_points2d(points)
 
 if __name__ == '__main__':
     main()
