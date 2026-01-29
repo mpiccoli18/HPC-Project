12c42225ad5483e5a61946660d1883a93e077a72
mpiccoli18 <mpiccoli18@gmail.com>
2026-01-29 11:19:04 +0100

    Added the correct dataset files with the generator script

--- tools/generator.py
+++ tools/generator.py
@@ -5,8 +5,8 @@ from sklearn.datasets import make_blobs
 
 os.makedirs('data/input', exist_ok=True)
 
-def generate_pow2_dataset(n_samples, n_features, centers, filename):
-    X, y = make_blobs(n_samples=n_samples, n_features=n_features, 
+def dataset(n_samples, n_features, centers, filename):
+    x, y = make_blobs(n_samples=n_samples, n_features=n_features,
                       centers=centers, cluster_std=0.6, random_state=42)
     
     df = pd.DataFrame(X, columns=['x', 'y', 'z'])
@@ -18,4 +18,4 @@ def generate_pow2_dataset(n_samples, n_features, centers, filename):
 
 pow2_sizes = [512, 1024, 2048, 4096, 8192, 16384, 32768]
 for size in pow2_sizes:
-    generate_pow2_dataset(size, 3, 5, f"test_{size}.csv")
+    dataset(size, 3, 5, f"test_{size}.csv")
