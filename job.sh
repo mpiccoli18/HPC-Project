67c8f242b888458a44b0456d9212c7b4db7608e6
mpiccoli18 <mpiccoli18@gmail.com>
2026-02-02 23:26:51 +0100

    Changed job.sh

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=1:mem=8gb -l place=scatter
-#PBS -l walltime=72:00:00
-#PBS -q long_cpuQ
+#PBS -l walltime=6:00:00
+#PBS -q shortCPUQ
 
 module purge
-module load gcc91
+module load GCC/11.2.0
 
 cd $PBS_O_WORKDIR
 datasets=("test_512.csv" "test_1024.csv" "test_2048.csv" "test_4096.csv" "test_8192.csv" "test_16384.csv")
