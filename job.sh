c49f9bb8b6a2eedebd5ef99a225e41ec2772de80
mpiccoli18 <mpiccoli18@gmail.com>
2026-02-03 13:57:51 +0100

    Added the datasets computed from the serial algorithm, now waiting for the 131072 point dataset

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=8gb -l place=scatter
+#PBS -l select=1:ncpus=1:mem=512gb -l place=scatter
 #PBS -l walltime=6:00:00
 #PBS -q shortCPUQ
 
@@ -7,7 +7,7 @@ module purge
 module load GCC/11.2.0
 
 cd $PBS_O_WORKDIR
-datasets=("test_512.csv" "test_1024.csv" "test_2048.csv" "test_4096.csv" "test_8192.csv" "test_16384.csv" "test_32768.csv" "test_65536.csv" "test_131072.csv")
+datasets=("test_131072.csv")
 
 echo "--- Serial Performance Benchmark ---"
 echo "Dataset, Time(s)"
