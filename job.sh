46b7ccf8a73a17804df5c1cda00148b90d8661da
mpiccoli18 <mpiccoli18@gmail.com>
2026-02-26 23:31:08 +0100

    More runs made

--- job.sh
+++ job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#PBS -l select=4:ncpus=16:mpiprocs=1:mem=256gb -l place=scatter:excl
-#PBS -l walltime=1:00:00
+#PBS -l select=32:ncpus=2:mpiprocs=1:mem=32gb -l place=pack:excl
+#PBS -l walltime=0:20:00
 #PBS -q shortCPUQ
 
 
@@ -11,18 +11,18 @@ module load OpenMPI/4.1.1-GCC-11.2.0
 cd $PBS_O_WORKDIR
 
 export PBS_O_PATH=$PATH
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=2
 export OMP_PROC_BIND=close
 export OMP_PLACES=cores
 export OMP_NESTED=false
 export OMP_MAX_ACTIVE_LEVELS=1
 
-datasets=("test_512.csv" "test_1024.csv" "test_2048.csv" "test_4096.csv" "test_8192.csv" "test_16384.csv" "test_32768.csv" "test_65536.csv")
-sigmas=("1.0" "0.6" "0.45" "0.35" "0.25" "0.2" "0.15" "0.1")
+datasets=("test_512.csv" "test_1024.csv" "test_2048.csv" "test_4096.csv" "test_8192.csv" "test_16384.csv" "test_32768.csv" "test_65536.csv" "test_131072.csv")
+sigmas=("1.0" "0.6" "0.45" "0.35" "0.25" "0.2" "0.15" "0.1" "0.1")
 
 datasets2=("test_131072.csv")
 
-echo "--- Parallel Performance Benchmark SCATTER ---" 
+echo "--- Parallel Performance Benchmark PACK:EXCL ---" 
 echo "Dataset, Time(s)"
 
 for i in "${!datasets[@]}"; do
@@ -37,5 +37,5 @@ for i in "${!datasets[@]}"; do
         --mca btl ^openib \
         --mca oob ^ud \
         --hostfile $PBS_NODEFILE \
-        -n 4 ./bin/spectral_clustering "$INPUT_PATH" "$OUTPUT_PATH" "$sigma"
+        -n 32 ./bin/spectral_clustering "$INPUT_PATH" "$OUTPUT_PATH" "$sigma"
 done
