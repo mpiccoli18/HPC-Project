cafad7cc28e7c6d43fd6debe2a9e1ca0b642cd82
mpiccoli18 <mpiccoli18@gmail.com>
2026-02-27 14:57:11 +0100

    Changed the runs with different configurations

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=1:mem=512gb -l place=scatter
+#PBS -l select=1:ncpus=1:mem=1024gb -l place=scatter:excl
 #PBS -l walltime=6:00:00
 #PBS -q shortCPUQ
 
@@ -9,7 +9,7 @@ module load GCC/11.2.0
 cd $PBS_O_WORKDIR
 datasets=("test_512.csv" "test_1024.csv" "test_2048.csv" "test_4096.csv" "test_8192.csv" "test_16384.csv" "test_32768.csv" "test_65536.csv" "test_131072.csv")
 
-echo "--- Serial Performance Benchmark ---"
+echo "--- Serial Performance Benchmark SCATTER:EXCL ---"
 echo "Dataset, Time(s)"
 
 for data in "${datasets[@]}"; do
